90c709fb4df6e50d6f21acd4eacf6ade71016619
JeongPyo <jeongpyo_park@tmax.co.kr>
2020-11-24 14:50:52 +0900

    call server api & get Data from server

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App(props) {
   // console.log(useState(null));
   var [_userName, setUserName] = useState(null);
 
+  useEffect(function() {
+    console.log('componenet did mount');
+    fetch('http://localhost:3001/api')
+            .then(res=>res.json())
+            .then(data=>setUserName(data.username));
+    // console.log(_userName)
+  },[]);
+
+  console.log('render');
   return (
     <div className="App">
       <header className="App-header">
